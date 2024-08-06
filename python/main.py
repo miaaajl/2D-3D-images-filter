3f483524932b75d6881dbf9485aa77bdf7657d47
miaaajl <liaojingyi112@gmail.com>
2024-08-06 21:10:56 +0100

    Add files via upload

---
+++ python/main.py
@@ -0,0 +1,7 @@
+
+from plot import plot_image_performance, plot_volume_performance, plot_kernel_performance
+
+if __name__ == "__main__":
+    # plot_image_performance("../data/image.csv")
+    # plot_volume_performance("../data/volume.csv")
+    plot_kernel_performance("../data/kernel.csv")
